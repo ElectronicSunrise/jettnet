bed2d1b65cd0bd47cfd099d59bbbcab70b4798a9
Andrew <andrew@andrew.andrew>
2021-10-25 21:37:28 -0700

    feat: add xunit and example test

---
+++ jettnet/jettnet/test/Core/CoreTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace jettnet.test.Core
+{
+    public class CoreTest
+    {
+        [Fact]
+        public void ToID_generatesId()
+        {
+            string instance = "I'm getting bricked up writing these unit tests";
+            var expected = 1895833957;
+            Assert.Equal(expected, instance.ToID());
+        }
+    }
+}
