a4a4d7846472a090e4f21d1b4bfdaa68476b3558
cxxpxr <60411087+cxxpxr@users.noreply.github.com>
2022-02-06 02:39:13 -0500

    Update JettConstants.cs

--- jettnet/jettnet/src/Core/JettConstants.cs
+++ jettnet/jettnet/src/Core/JettConstants.cs
@@ -5,7 +5,7 @@
         public const  int DefaultBufferSize = 1200 - OVERHEAD;
         private const int OVERHEAD          = sizeof(int);
 
-        public const int ReliableChannel = 0;
-        public const int Unreliable      = 1;
+        public const int ReliableChannel   = 0;
+        public const int UnreliableChannel = 1;
     }
 }
